a0499ddb3690db71c9c9e9eb24345ca9424e5cab
Alex <33594597+AlexeyGorbunov@users.noreply.github.com>
2019-07-09 12:43:50 +0300

    Add files via upload

---
+++ WebCacheCleaner.swift
@@ -0,0 +1,35 @@
+//
+//  WebCacheCleaner.swift
+//  SupremeBot
+//
+//  Created by Алексей Горбунов on 09/07/2019.
+//  Copyright © 2019 Алексей Горбунов. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+final class WebCacheCleaner {
+    
+    class func clean(domen: [String]? = nil) {
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+        print("[WebCacheCleaner] All cookies deleted")
+        
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            if let name = domen {
+                let arrayDataSite = records.filter { name.contains($0.displayName)}
+                deleteData(records: arrayDataSite)
+            } else {
+                deleteData(records: records)
+            }
+        }
+    }
+    
+    static private func deleteData(records: [WKWebsiteDataRecord]) {
+        records.forEach { record in
+            WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+            print("[WebCacheCleaner] Record \(record) deleted")
+        }
+    }
+    
+}
